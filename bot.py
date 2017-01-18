ad2eef89c2db7b8f438e23d50ffb8a21a4d79d24
aqua-rar <spamies@live.com>
2017-01-18 23:32:32 +0000

    fixed MARINS BUGS AGAIN AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
    i hope you had a good night marin

--- bot.py
+++ bot.py
@@ -314,7 +314,7 @@ class Client(discord.Client):
 
             drools = 1
             if len(split) > 1 and split[1].isdigit():
-                drools = split[1]
+                drools = int(split[1])
             elif len(split) > 1 and split[1].lower() == "all":
                 drools = self.user_data[message.author.id]["dubs"]["rolls"]
 
@@ -322,53 +322,55 @@ class Client(discord.Client):
 
 
                 self.user_data[message.author.id]["dubs"]["rolls"] -= drools
-                wins_shortened = {"0":"***WOWE! LOWEST ROL POSSIBLE!!***\n","1":"*dubs*","2":"**trips**","3":"***quads***",
-                                  "4":"quints!":"5":"***WHAT TO HECK, SEXTUPLES***","6":"***WOWE! HIHGEST ROL POSSIBLE!!***\n"}
+                wins_shortened = {0:"No dubs :frowning:",1:"*dubs*",2:"**trips**",3:"***quads***",
+                                  4:"quints!",5:"***WHAT TO HECK, SEXTUPLES***",6:"***WOWE! LOWEST ROL POSSIBLE!!***",7:"***WOWE! HIHGEST ROL POSSIBLE!!***\n"}
 
                 wins = {
-                        0: 'LOWEST', 1: 'No dubs :frowning:', 2: "Dubs! Nice! Check'em!", 3: 'Trips?! Whoa, check those bad boys!',
-                        4: 'Quads?! No way! Witnessed!', 5: "PENTS?! HOT DAMN! Now that's #rare.", 6: 'TOO MANY REPEATING DIGITS. GET THIS PERSON A MEDAL!',
-                        7: 'HIGHEST'
+                        0: 'No dubs :frowning:', 1: "Dubs! Nice! Check'em!", 2: 'Trips?! Whoa, check those bad boys!',
+                        3: 'Quads?! No way! Witnessed!', 4: "PENTS?! HOT DAMN! Now that's #rare.", 5: 'TOO MANY REPEATING DIGITS. GET THIS PERSON A MEDAL!',
+                        6: 'LOWEST', 7:"HIGHEST"
                         }
                 numdict = {
                             "1" : "one","2" : "two","3" : "three","4" : "four","5" : "five",
                             "6" : "six","7" : "seven","8" : "eight","9" : "nine", "0":"zero"
                           }
                 prize_dict = {
-                              1: 1, 2: 10, 3: 100,
-                              4: 250, 5: 500, 6: 750
+                              0: 1, 1: 10, 2: 100,
+                              3: 250, 4: 500, 5: 750
                               }
-
 
                 if drools > 1:
                     finalprize, out = 0, ""
                     for i in range(0,drools):
                         roll = str(random.randint(0,1000000))
                         if roll in (0,1000000):
-                            out        += wins_shortened[mappu((int(roll),0,1000000),(0,7))]
+                            out        += wins_shortened[mappu((int(roll),0,1000000),(6,7))]
                             finalprize += int(self.user_data[message.author.id]["dubs"]["level"] * 1000)
                             break
-                        for x in roll[::-1]:
-                            if roll[x] == roll[x+1]:
-                                win += 1
-                            else: break
+                        else:
+                            win = 0
 
-                        out         += "{} {} : {}\n".format(i+'. ',wins_shortened[win], ''.join([":{}:".format(numdict[i]) for i in roll]))
-                        finalprize  += prize_dict[int(i)]*self.user_data[message.author.id]["dubs"]["level"]
+                            for i in range(-1, -len(roll), -1):
+                                if roll[i] == roll[i-1]: win += 1
+                                else:                    break
+
+                            out         += "{2} roll {0} {1}\n".format(str(i)+'. ',wins_shortened[win], ''.join([":{}:".format(numdict[i]) for i in roll]))
+                            finalprize  += prize_dict[win]*self.user_data[message.author.id]["dubs"]["level"]
+                    finalprize = int(finalprize)
                     out+="You won {} dabs {} total!".format(finalprize, DAB_EMOJI)
+                    self.user_data[message.author.id]["money"] += finalprize
                     yield from self.send_message(message.channel, out)
-                    self.user_data[message.author.id]["money"] += totalprize
 
                 else:
 
                     roll = str(random.randint(0,1000000))
-                    win = 1
+                    win = 0
 
                     for i in range(-1, -len(roll), -1):
                         if roll[i] == roll[i-1]: win += 1
                         else:                    break
 
-                    yield from self.send_message(message.channel, ''.join([numdict[i] for i in roll]))
+                    yield from self.send_message(message.channel, ''.join([":{}:".format(numdict[i]) for i in roll]))
                     if roll in ("0","1000000"):
                         msg = "WOW! {} POSSIBLE ROLL! What does this mean?".format(win[mappu((int(roll),0,1000000),(0,7))])
                         prize = int(self.user_data[message.author.id]["dubs"]["level"] * 1000)
