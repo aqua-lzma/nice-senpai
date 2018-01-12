764312f16ebf9cb0148be031a1efd5688e656163
aqua-rar <spamies@live.com>
2018-01-12 11:05:17 +0000

    Fix scope
    Gosh darn re-used variable names

--- commands/_update_dabs.js
+++ commands/_update_dabs.js
@@ -1,7 +1,7 @@
-module.exports = function update_dabs(user, config, amount) {
-    user = config.users[user.id]
+module.exports = function update_dabs(member, config, amount) {
+    user = config.users[member.id]
     if (user === undefined) {
-        user = config.users[user.id] = {
+        user = config.users[member.id] = {
             dabs: 100,
             dab_record: 100,
             level: 1,
@@ -13,7 +13,7 @@ module.exports = function update_dabs(user, config, amount) {
     if (user.daily_claim != new Date().getDay()) {
         user.daily_claim = new Date().getDay()
         user.daily_rolls += Math.floor(10 * (Math.log(user.level) / Math.log(5))) + 1
-        message.channel.send(`${user.username} has new daily rolls!`)
+        message.channel.send(`${member.username} has new daily rolls!`)
     }
     if (user.dabs > user.dab_record) user.dab_record = user.dabs
     return user
