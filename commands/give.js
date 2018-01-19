26bb755ad9aa38713459cd6d2f2b16480611deac
Hakurei Reimu <gmike.travis@gmail.com>
2018-01-19 20:57:23 +0800

    implemented give

--- commands/give.js
+++ commands/give.js
@@ -1,11 +1,36 @@
 module.exports = {
     title: "Give",
-    desc: "Complain to @March#7572 for this not being implemented yet.",
-    syntax: "`TODO`",
+    desc: "Give mentioned users <amount> dabs. Positive amounts only.",
+    syntax: "`{prefix}give <amount> [user-mentions ...]",
     alias: ["give"],
     owner_only: false,
     affect_config: false,
     action: function(message, config) {
-        message.channel.send("Try `$help give`.")
+        message_words = message.content.split(" ")
+
+        for (word of message_words) {
+            if (!Number.isNaN(Number(word)))
+                amount = Number(word)
+        }
+
+        if (amount === undefined)
+            return message.channel.send("Specify an amount. No dabs given.")
+        if (amount <= 0 || (Number.isInteger == false))
+            return message.channel.send("Specify a valid amount. No dabs given.")
+
+        sender = message.author
+        receivers = message.mentions.users
+        user = update_dabs(sender, config)
+        total_amount = amount * receivers.size
+        if (user.dabs < total_amount)
+            return message.channel.send("Not enough dabs to give. No dabs given.")
+
+        for (receiver of receivers.values()) {
+            update_dabs(sender, config, -1 * amount)
+            update_dabs(receiver, config, amount)
+        }
+
+        message.channel.send("Sent " + total_amount + " dabs to " + 
+            receivers.size + " users")
     }
 }
