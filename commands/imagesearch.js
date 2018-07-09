46671bdece591185a38d6645a32835f7a15d2c1e
愀焀甀愀ⴀ爀愀爀 <spamies@live.com>
2018-07-09 13:20:22 +0100

    Update imagesearch.js
    Can now do multiple searches at the same time because `query` is no longer a global variable.
    Can also now search for special characters because we're doing `encodeURIComponent`.
    Added random top 20 results functionality.
    Added rewind and fast forward buttons to skip 10.

--- commands/imagesearch.js
+++ commands/imagesearch.js
@@ -1,56 +1,75 @@
-request = require("request")
+const request = require('request')
 
 module.exports = {
-    title: "Search image",
-    desc: "Search googlel for an image, will be a random one from the top 100 results.\n" +
-          "*NotSoBot eat your heart out.*",
-    syntax: "`{prefix}im <image query>` where query is what you want to search for.",
-    alias: ["i", "im", "img", "imag", "image"],
-    owner_only: false,
-    affect_config: false,
-    action: function(message, config) {
-        query = message.content.split(" ").slice(1).join(" ")
-        if (query == "") return message.channel.send("Cannot search blank query.")
-        req = {
-            url: `https://www.google.co.uk/search?q=${query}&tbm=isch`,
-            headers: { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0" }
-        }
-        request.get(req, (err, res, body) => {
-            if (err) return message.channel.send(`Error:\`\`\`\n${String(err)}\`\`\``)
-            images = body.match(/(?<="ou":")[^"]*(?=")/g)
-            index = 0
+  title: 'Search image',
+  desc: [
+    'Search googlel for an image, will be the top 100 results sorted by relevance.',
+    'Or use i2 to search for an a random image in the top 20 results.',
+    'Any of the aliases ending with 2 is valid for a random search.',
+    'Bot will stop listening for reactions after 30 seconds.'
+  ].join('\n'),
+  syntax: [
+    '`{prefix}im <image query>` where query is what you want to search for.',
+    '`{prefix}im2 <image query>` same as above but will return a random result from top 20.'
+  ].join('\n'),
+  alias: ['i', 'im', 'img', 'imag', 'image', 'i2', 'im2', 'img2', 'imag2', 'image2'],
+  owner_only: false,
+  affect_config: false,
+  action: function (message) {
+    var query = message.content.split(' ').slice(1).join(' ')
+    if (query === '') return message.channel.send('Cannot search blank query.')
+    var req = {
+      url: `https://www.google.co.uk/search?q=${encodeURIComponent(query)}&tbm=isch`,
+      headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0' }
+    }
+    request.get(req, (err, res, body) => {
+      if (err) return message.channel.send(`Error:\`\`\`\n${String(err)}\`\`\``)
+      var images = body.match(/"ou":"([^"]*)"/g).map(i => i.slice(6, -1))
+      var index = 0
 
-            function awaitReactions(response) {
-                response.react("◀")
-                .then(() => response.react("▶")
-                .then(() => response.react("🔀")
-                .then(() => {
-                    response.createReactionCollector(
-                        (reaction, user) => ["◀", "▶", "🔀"].indexOf(reaction.emoji.name) >= 0 && user.id == message.author.id,
-                        { max: 1, time: 30000 }
-                    ).on("collect", reaction => {
-                        if (reaction.emoji.name == "◀") index--
-                        else if (reaction.emoji.name == "▶") index++
-                        else if (reaction.emoji.name == "🔀") index = Math.floor(Math.random() * images.length)
-                        index = ((index % 100) + 100) % 100
-                        response.edit("", {embed: {
-                            title: query + `: ${index}`,
-                            description: images[index],
-                            image: { url: images[index] }
-                        }})
-                        response.clearReactions().then(awaitReactions)
-                    }).on("end", (collected, reason) => {
-                        if (reason != "limit") response.clearReactions()
-                    })
-                })))
-            }
+      if (message.content.split(' ')[0].slice(-1) === '2') {
+        index = Math.floor(Math.random() * Math.floor(images.length / 5))
+        return message.channel.send('', {embed: {
+          title: query + `: ${index}`,
+          description: images[index],
+          image: { url: images[index] }
+        }})
+      }
 
-            message.channel.send("", {embed: {
+      function awaitReactions (response) {
+        response.react('⏪')
+          .then(() => response.react('◀'))
+          .then(() => response.react('▶'))
+          .then(() => response.react('⏩'))
+          .then(() => response.react('🔀'))
+          .then(() => {
+            response.createReactionCollector(
+              (reaction, user) => ['⏪', '◀', '▶', '⏩', '🔀'].indexOf(reaction.emoji.name) >= 0 && user.id === message.author.id,
+              { max: 1, time: 30000 }
+            ).on('collect', reaction => {
+              if (reaction.emoji.name === '⏪') index -= 10
+              else if (reaction.emoji.name === '◀') index--
+              else if (reaction.emoji.name === '▶') index++
+              else if (reaction.emoji.name === '⏩') index += 10
+              else if (reaction.emoji.name === '🔀') index = Math.floor(Math.random() * images.length)
+              index = ((index % 100) + 100) % 100
+              response.edit('', {embed: {
                 title: query + `: ${index}`,
                 description: images[index],
                 image: { url: images[index] }
-            }})
-            .then(awaitReactions)
-        })
-    }
+              }})
+              response.clearReactions().then(awaitReactions)
+            }).on('end', (collected, reason) => {
+              if (reason !== 'limit') response.clearReactions()
+            })
+          })
+      }
+
+      message.channel.send('', {embed: {
+        title: query + `: ${index}`,
+        description: images[index],
+        image: { url: images[index] }
+      }}).then(awaitReactions)
+    })
+  }
 }
