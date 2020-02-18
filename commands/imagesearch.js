3b491162dabd72517fd52d65cf32c36ae6f626f0
پک¤پK“©”پ <spamies@live.com>
2020-02-18 16:04:38 +0000

    Fix to parse googles new image output
    Them fuckers better not change it again.
    Also it can delete a search now.

--- commands/imagesearch.js
+++ commands/imagesearch.js
@@ -23,22 +23,25 @@ module.exports = {
       headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0' }
     }
     request.get(req, (err, res, body) => {
+      require('fs').writeFileSync('test.html', body)
       if (err) return message.channel.send(`Error:\`\`\`\n${String(err)}\`\`\``)
-      var images = body.match(/"ou":"([^"]*)"/g).map(i => i.slice(6, -1))
+      var images = body.match(/\["http?s:\/\/(?!encrypted[^.]+\.gstatic)[^"]+"/g).map(i => i.slice(2, -1))
       var index = 0
 
       if (message.content.split(' ')[0].slice(-1) === '2') {
         index = Math.floor(Math.random() * Math.floor(images.length / 5))
-        return message.channel.send('', {embed: {
-          title: query + `: ${index}`,
-          description: images[index],
-          image: { url: images[index] }
-        }})
+        return message.channel.send('', {
+          embed: {
+            title: query + `: ${index}`,
+            description: images[index],
+            image: { url: images[index] }
+          }
+        })
       }
 
       function awaitReactions (response) {
         response.createReactionCollector(
-          (reaction, user) => ['⏪', '◀', '▶', '⏩', '🔀'].indexOf(reaction.emoji.name) >= 0 && user.id === message.author.id,
+          (reaction, user) => ['⏪', '◀', '▶', '⏩', '🔀', '⏹️'].indexOf(reaction.emoji.name) >= 0 && user.id === message.author.id,
           { max: 1, time: 30000 }
         ).on('collect', reaction => {
           if (reaction.emoji.name === '⏪') index -= 10
@@ -46,29 +49,35 @@ module.exports = {
           else if (reaction.emoji.name === '▶') index++
           else if (reaction.emoji.name === '⏩') index += 10
           else if (reaction.emoji.name === '🔀') index = Math.floor(Math.random() * images.length)
+          else if (reaction.emoji.name === '⏹️') return response.delete()
           index = ((index % 100) + 100) % 100
-          response.edit('', {embed: {
-            title: query + `: ${index}`,
-            description: images[index],
-            image: { url: images[index] }
-          }})
+          response.edit('', {
+            embed: {
+              title: query + `: ${index}`,
+              description: images[index],
+              image: { url: images[index] }
+            }
+          })
           reaction.remove(message.author).then(() => awaitReactions(response))
         }).on('end', (collected, reason) => {
           if (reason !== 'limit') response.clearReactions()
         })
       }
 
-      message.channel.send('', {embed: {
-        title: query + `: ${index}`,
-        description: images[index],
-        image: { url: images[index] }
-      }}).then(response => {
+      message.channel.send('', {
+        embed: {
+          title: query + `: ${index}`,
+          description: images[index],
+          image: { url: images[index] }
+        }
+      }).then(response => {
         response
           .react('⏪')
           .then(() => response.react('◀'))
           .then(() => response.react('▶'))
           .then(() => response.react('⏩'))
           .then(() => response.react('🔀'))
+          .then(() => response.react('⏹️'))
           .then(() => awaitReactions(response))
       })
     })
